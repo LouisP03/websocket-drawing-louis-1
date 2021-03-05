16c4dd19e4c3580b4950642423cdfc76572ee092
Louis Parkes <67322709+LouisP03@users.noreply.github.com>
2021-03-05 18:19:47 +0000

    adjusted framerate

--- public/sketch.js
+++ public/sketch.js
@@ -21,7 +21,7 @@ chosenColour = {
 };
 
 function setup() {
-	frameRate(10);
+	frameRate(60);
 	const elements = document.querySelectorAll(".colourSlider");
 	elements.forEach(element => {
 	  element.addEventListener('input', (e) => {
